f9f6256c8961219c7f7e7253219a6f27fb074a21
HannahYudkin <hannahyudkin@gmail.com>
2020-06-27 09:55:47 -0400

    Created custom hook.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import Searchparams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Dash",
-  //     animal: "Dog",
-  //     breed: "Dachschund",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Whitey",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Kiwi",
-  //     animal: "Turtle",
-  //     breed: "Snapping?",
-  //   }),
-  // ]);
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Dash" animal="Dog" breed="Dachschund" />
-      <Pet name="Whitey" animal="Cat" breed="Mixed" />
-      <Pet name="Kiwi" animal="Turtle" breed="Snapping?" />
+      <Searchparams />
     </div>
   );
 };
